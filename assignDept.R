f975b04b7f2bbe0438603f8826c753990586bcac
athenry <alisontaylorhenry@gmail.com>
2017-10-14 21:17:31 -0600

    Improved mapping of department names.

---
+++ assignDept.R
@@ -0,0 +1,9 @@
+## Testing an improved procedure for assigning Departmental affiliations for each articles
+
+deptUrl <- ("https://docs.google.com/spreadsheets/d/e/2PACX-1vTMpIJn2N9pV13zRhYKRdOOAUfvHhKF6dqUzMWhnk3_eaBgPD8XT6UJBuAXfyoWfA0qfvaO4LyQpfJA/pub?gid=0&single=true&output=csv")
+depts <- read.csv(deptUrl)
+abs <- as.character(depts$Abbreviation)
+testData <- tidy_data[grep("UNIV ALBERTA", tidy_data$C1), ]
+testData$Department <- ifelse(grepl(paste(abs, collapse = "|"), testData$C1), abs, "Other")
+testData$Department <- str_replace(as.character(testData$Department), as.character(depts$Abbreviation), as.character(depts$Name))
+
