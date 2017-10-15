79724e0f3fe82117d84eebe636ffd5edb4a6ba81
athenry <alisontaylorhenry@gmail.com>
2017-10-14 22:46:03 -0600

    does not work.

--- assignDept.R
+++ assignDept.R
@@ -1,4 +1,5 @@
 ## Testing an improved procedure for assigning Departmental affiliations for each articles
+## As-is DOES NOT WORK
 
 deptUrl <- ("https://docs.google.com/spreadsheets/d/e/2PACX-1vTMpIJn2N9pV13zRhYKRdOOAUfvHhKF6dqUzMWhnk3_eaBgPD8XT6UJBuAXfyoWfA0qfvaO4LyQpfJA/pub?gid=0&single=true&output=csv")
 depts <- read.csv(deptUrl)
