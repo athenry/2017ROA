7a11c5d58ff4661a9cef7315fb0973cc1deec0a7
Henry <ahenry@ualibrary.ualberta.ca>
2017-10-19 15:11:03 -0600

    almost have department assignment fixed. Saving before updating.

--- assignDept.R
+++ assignDept.R
@@ -1,10 +1,19 @@
 ## Testing an improved procedure for assigning Departmental affiliations for each articles
-## As-is DOES NOT WORK
 
+AlbertaData <- tidy_data1[grep("UNIV ALBERTA", tidy_data1$C1), ]
+engData <- AlbertaData[grep("ENGN", AlbertaData$C1), ]
+
+## Option 1
 deptUrl <- ("https://docs.google.com/spreadsheets/d/e/2PACX-1vTMpIJn2N9pV13zRhYKRdOOAUfvHhKF6dqUzMWhnk3_eaBgPD8XT6UJBuAXfyoWfA0qfvaO4LyQpfJA/pub?gid=0&single=true&output=csv")
 depts <- read.csv(deptUrl)
 abs <- as.character(depts$Abbreviation)
-testData <- tidy_data[grep("UNIV ALBERTA", tidy_data$C1), ]
-testData$Department <- ifelse(grepl(paste(abs, collapse = "|"), testData$C1), abs, "Other")
-testData$Department <- str_replace(as.character(testData$Department), as.character(depts$Abbreviation), as.character(depts$Name))
+testData <- engData
+
+testData$Abbreviation <- as.character(sapply(testData$C1, function(x) abs[str_detect(x, abs)]))
+
+## problem with this solution: anything with multiple matches gets NA
+finalTestData <- merge(testData, depts, all.x = TRUE) ##keeps nonmatches and enters NA
+
+
+
 
