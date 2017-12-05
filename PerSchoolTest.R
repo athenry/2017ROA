43b2e8eea77d07a6a9b6fa0c5a766aeb07350aed
Henry <ahenry@ualibrary.ualberta.ca>
2017-12-04 20:53:43 -0700

    exporting new data

--- PerSchoolTest.R
+++ PerSchoolTest.R
@@ -8,17 +8,20 @@ USTC <- rbind(biomedUS, chemmatUS, civilUS, elecUS, mechUS) %>%
         group_by(School) %>%
         summarise('Times Cited' = mean(TC))
 
-CanComp <- rbind(biomed, chemmat, civil, electrical, mechanical) %>%
-        count(PY, School) %>%
-        spread(PY, n)
-
 CanFac <- rbind(chemmatFac, civilFac, elecFac, mechFac) %>%
         gather(key = PY, value = Fac, -School, na.rm = TRUE) %>%
         group_by(School, PY) %>%
         summarise(Fac = sum(Fac)) %>%
         spread(PY, Fac)
 
-UScomp <- rbind(biomedUS, chemmatUS, civilUS, elecUS, mechUS) %>%
+CanComp <- rbind(biomed, chemmat, civil, electrical, mechanical) %>%
+        count(PY, School) %>%
+        spread(PY, n) %>%
+        filter(School %in% CanFac$School)
+
+CanAPF <- cbind(CanComp[1], round(CanComp[-1]/CanFac[-1],1)) 
+
+UScomp <- rbind(chemmatUS, civilUS, elecUS, mechUS) %>%
         count(PY, School) %>%
         spread(PY, n)
 
@@ -27,4 +30,10 @@ USfac <- select(rbind(chemmatFacUS, civilFacUS, elecFacUS, mechFacUS), -Departme
         group_by(School, PY) %>%
         summarise(Fac = sum(Fac)) %>%
         spread(PY, Fac)
-        
+
+USapf <- cbind(UScomp[1], round(UScomp[-1]/USfac[-1],1))
+
+write.csv(CanTC, "CanTC.csv", quote = TRUE, row.names = FALSE)
+write.csv(USTC, "USTC.csv", quote = TRUE, row.names = FALSE)
+write.csv(CanAPF, "CanAPF", quote = TRUE, row.names = FALSE)
+write.csv(USapf, "USapf.csv", quote = TRUE, row.names = FALSE)
